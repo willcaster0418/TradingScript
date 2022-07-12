0933d8222e1fe74f8a4cd40f89ccd3084afd3527
KSH <fury8208@gmail.com>
2022-07-12 19:38:14 +0900

    interface for oms

---
+++ oms/__init__.py
@@ -0,0 +1,23 @@
+class STOrder:
+    def __init__(self):
+        return None
+
+class STBalance:
+    def __init__(self):
+        return None
+
+class STAccount:
+    def __init__(self):
+        return None
+
+class STOrderManager:
+    def __init__(self):
+        return None
+    def execute(self, order):
+        return None
+    def confirm(self, order):
+        return None
+    def query(self, info):
+        return None
+    def synchronize(self, order):
+        return None
