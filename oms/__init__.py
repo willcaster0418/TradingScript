69aac4bd9c2c3654545a039ffafd45f51ea673ad
KSH <fury8208@gmail.com>
2022-07-12 19:46:08 +0900

    change oms interface

--- oms/__init__.py
+++ oms/__init__.py
@@ -1,23 +1,35 @@
-class STOrder:
+class Order:
     def __init__(self):
         return None
 
-class STBalance:
+class OrderEvent:
     def __init__(self):
         return None
 
-class STAccount:
+class Balance:
     def __init__(self):
         return None
 
-class STOrderManager:
+class Account:
     def __init__(self):
         return None
+
+class OrderManager:
+    def __init__(self):
+        self.orders = {}
+        self.order_events = {}
+        self.balances = {}
+        self.accounts = {}
+        return None
+        
     def execute(self, order):
         return None
-    def confirm(self, order):
-        return None
+
     def query(self, info):
         return None
+
+    def confirm(self, order):
+        return None
+
     def synchronize(self, order):
         return None
